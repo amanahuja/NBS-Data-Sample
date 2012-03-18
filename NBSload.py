42fe9ea77bf4655ec7f6dfdf1b2595f8b1e85ad5
Aman Ahuja <amanahuja@gmail.com>
2012-03-18 16:25:36 -0700

    Create a loop through csv files

--- NBSload.py
+++ NBSload.py
@@ -6,3 +6,21 @@ Loading the NBS data into Pandas
 @author: Aman
 """
 
+#Move to working directory
+import os
+os.chdir('C:\Users\Aman\Documents\Projects\NextBigSound\NBS-Data-Sample')
+
+#Set data directory
+datadir = 'data'
+path = os.path.join(os.getcwd(), datadir)
+
+#Loop through each file in the data directory
+for filename in os.listdir(path):
+    #Ignore non-csv files, such as the readme.txt
+    
+    '''
+    For each data file, load the data into a dataframe
+    Then concat the file's data into a master dataframe
+    '''
+    pass
+
