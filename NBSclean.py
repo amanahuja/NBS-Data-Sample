dbe49afd3b51d119a5ea8f67d8c723561a3fbb17
Aman Ahuja <amanahuja@gmail.com>
2012-03-18 17:45:59 -0700

    Create a basic df with a multi-index. Omitting empty columns, Event columns, and some others.

--- NBSclean.py
+++ NBSclean.py
@@ -19,12 +19,39 @@ from pandas import *
 #Retrieve dataframes from the hdf5 file
 store = HDFStore(os.path.join(path, 'NBSData.h5'))
 masterdf = store['master']
+
 '''
-Process the dataframes here
+Process and clean the dataframes
 '''
-print masterdf.shape
+#the good stuff
+basicdf = masterdf[['Artist.ID','Day', 'DayOfWeek', 'Facebook.fans.d', 'Facebook.fans.t', 'Last.fm.plays.d', 'Last.fm.plays.t',
+		    'MySpace.fans.d', 'MySpace.fans.t', 'MySpace.plays.d', 'MySpace.plays.t', 'Twitter.fans.d', 'Twitter.fans.t',
+		    'Twitter.statuses.d', 'Twitter.statuses.t', 'YouTube.fans.d', 'YouTube.fans.t', 'YouTube.plays.d', 'YouTube.plays.t',
+		    'iTunes.Album.Units.d', 'iTunes.Track.Units.d', 'MediaGuide.Radio.Spins.d', 'Pandora.fans.d', 'Pandora.fans.t',
+		    'Rdio.fans.d', 'Rdio.fans.t', 'Rdio.plays.d', 'Rdio.plays.t', 'SiteCatalyst.Visits.d', 'SoundCloud.fans.d',
+		    'SoundCloud.fans.t', 'SoundCloud.plays.d', 'SoundCloud.plays.t', 'Spotify.plays.d', 'Vevo.plays.d', 'Vevo.plays.t',
+		    'Wikipedia.views.d']]
+    
+
+#Create a multi Index
+from datetime import datetime
+
+idx_time = basicdf.Day * 86400
+idx_time = idx_time.apply(datetime.fromtimestamp)
+idx_artist = basicdf['Artist.ID']
+
+idx = MultiIndex.from_tuples(zip(idx_time,idx_artist), 
+                             names=['date', 'artist'])
+
+
+
+#Apply the Multi-index
+cols = basicdf.columns
+rawdata = basicdf.as_matrix()
+basicdf = DataFrame(rawdata, columns = cols, index = idx)
 
 #Save the dataframes back into hdf5 file. 
+store['basic'] = basicdf
 
 #Close the hdf5 file. 
 store.close()
